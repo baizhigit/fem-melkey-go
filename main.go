f026109335703eb85c1854cf4c97d3cf9d981f2c
Yonko Shanks <kbaizhigit@gmail.com>
2026-04-19 01:27:47 +0500

    18

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
-import "github.com/baizhigit/fem-melkey-go/internal/app"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/baizhigit/fem-melkey-go/internal/app"
+)
 
 func main() {
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
+
 	app.Logger.Println("we are running out app")
+
+	http.HandleFunc("/health", HealthCheck)
+
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
 }
